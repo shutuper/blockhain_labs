dedf2ed807d97aa12403db00f16630fd9a63df3e
Yehor Hnitii <yhnitii@osdb.io>
2022-10-23 19:17:06 +0300

    5 lab;

--- src/main/java/com/ia01/hnitii/smo/Main.java
+++ src/main/java/com/ia01/hnitii/smo/Main.java
@@ -2,91 +2,98 @@ package com.ia01.hnitii.smo;
 
 import java.util.Arrays;
 
-import static java.lang.Math.min;
+import static java.lang.Math.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		int n = 4, m = 10, lambda = 35;
-		double u = 8, p = lambda / u;
+		int n = 4, m = 10, lambda = 5;
+		double u = 1D / 0.18, p = lambda / u;
 
-		double[] pT = smo_2(n, m, p);
-		System.out.printf("smo_2(n = %s, m = %s, p = %s) = \npT%s\n", n, m, p, Arrays.toString(pT));
+		double[] pT = smo_2_b(n, m, p);
+		System.out.printf("smo_2_b(n = %s, m = %s, p = %s) = \npT%s\n", n, m, p, Arrays.toString(pT));
 
-		printCharacteristics(n, m, lambda, u, pT);
-		printUWhenRejectPLTE(n, m, lambda, 0.1);
+		printCharacteristics(n, m, lambda, u, pT, 10);
+		printUWhenRejectPLTE(n, m, lambda);
 	}
 
-	private static double[] smo_2(int n, int m, double p) {
+	private static double[] smo_2_b(int n, int m, double p) {
 		int len = n + m + 1;
 		double[] pT = new double[len];
-		double[] q = new double[len];
-		q[0] = 1;
+		pT[0] = 1;
 		double s = 1;
 
 		for (int k = 1; k < len; k++) {
-			q[k] = q[k - 1] * p / min(k, n);
-			s += q[k];
+			pT[k] = pT[k - 1] * p / min(k, n);
+			if (k > n) {
+				pT[k] *= pow(1d / n, k - n);
+			}
+			s += pT[k];
 		}
 
 		pT[0] = 1 / s;
 
 		for (int k = 1; k < len; k++) {
-			pT[k] = q[k] * pT[0];
+			pT[k] *= pT[0];
 		}
 
 		return pT;
 	}
 
-	private static void printCharacteristics(int n, int m, int lambda, double u, double[] pT) {
+	private static void printCharacteristics(int n, int m, int lambda, double u, double[] pT, int maxLen) {
 		double p = pT[pT.length - 1];
 		double q = 1 - p;
-		double A = lambda * q;
 
-		double k3 = 0;
-		for (int k = 0; k <= n + m; k++) {
-			k3 += min(k, n) * pT[k];
+		double avgBusyN = 0;
+		for (int k = 1; k <= n && k < pT.length; k++) {
+			avgBusyN += k * pT[k];
 		}
 
-		double n4 = 0;
-		for (int k = 1; k <= m; k++) {
-			n4 += k * pT[n + k];
+		double avgQueueLen = 0;
+		for (int k = 1; (n + k < pT.length) && (k <= maxLen); k++) {
+			avgQueueLen += k * pT[n + k];
 		}
 
-		double nc = k3 + n4;
-		double t4ek = n4 / lambda;
-		double tc = t4ek + 1 / u;
+		double queueExistence = 0;
+		for (int k = 1; (n + k < pT.length) && (k <= maxLen); k++) {
+			queueExistence += pT[n + k];
+		}
 
 		double P3 = 0;
-		for (int k = 0; k <= m; k++) {
+		for (int k = 0; (n + k < pT.length) && (k <= maxLen); k++) {
 			P3 += pT[n + k];
 		}
 
 		double P4 = 0;
-		for (int k = 1; k <= m; k++) {
-			P4 += pT[n + k];
+		for (int k = n; k < pT.length; k++) {
+			P4 += pT[k];
 		}
 
-		System.out.println("\nOperational printCharacteristics:\n");
+		System.out.println("\nОпераційні характеристики СМО:\n");
 		System.out.println("Pвідм = " + p);
-		System.out.println("q = " + q);
-		System.out.println("A = " + A);
-		System.out.println("kз = " + k3);
-		System.out.println("nчер = " + n4);
-		System.out.println("nc = " + nc);
-		System.out.println("tчек = " + t4ek);
-		System.out.println("tс = " + tc);
-		System.out.println("Pз = " + P3);
-		System.out.println("Pчер = " + P4);
+		System.out.println("Середня кількість зайнятих каналів = " + avgBusyN);
+		System.out.println("Середня довжина черги = " + avgQueueLen);
+		System.out.println("Середня кількість повідомлень = " + (avgBusyN + avgQueueLen));
+		System.out.println("Середній час чекання у черзі = " + (avgQueueLen / lambda));
+		System.out.println("Середній час перебування SMS у системі = " + (avgQueueLen / lambda + q / u));
+		System.out.println("Імовірність наявності черги = " + queueExistence);
+		System.out.println("Імовірність зайнятості каналів обслуговування = " + P3);
+		System.out.println("Імовірність чекання SMS у черзі = " + P4);
 	}
 
-	private static void printUWhenRejectPLTE(int n, int m, double lambda, double lte) {
-		for (int i = 1; ; i++) {
-			double[] pT = smo_2(n, m, lambda / i);
-			double rejectP = pT[pT.length - 1];
+	private static void printUWhenRejectPLTE(int n, int m, int lambda) {
+		for (double t = 2.55; t > 0; t -= 0.01D) {
+			double u = lambda / (1D / t);
+			double[] pT = smo_2_b(n, m * 10, u);
 
-			if (rejectP <= lte) {
-				System.out.println("\nP(відмови) <= " + lte + " при u >= " + i);
+			double avgQueueLen = 0;
+			for (int k = 1; k <= m; k++) {
+				avgQueueLen += k * pT[n + k];
+			}
+
+			if (((int) avgQueueLen) <= m + 1) {
+				System.out.println("\nМінімальний час обробки SMS-повідомлень, щоб черга не перевищувала "
+						+ m + " повідомлень: " + (t + 0.01) + "хв");
 				return;
 			}
 		}
