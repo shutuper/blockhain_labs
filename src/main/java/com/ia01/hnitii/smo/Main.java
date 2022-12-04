207354bf2bc531567dfb72f2b25adcf18645cca4
Yehor Hnitii <yhnitii@osdb.io>
2022-12-04 16:07:46 +0200

    8 lab;

--- src/main/java/com/ia01/hnitii/smo/Main.java
+++ src/main/java/com/ia01/hnitii/smo/Main.java
@@ -2,34 +2,53 @@ package com.ia01.hnitii.smo;
 
 import java.util.Arrays;
 
-import static java.lang.Math.*;
+import static java.lang.Math.min;
+import static java.lang.Math.pow;
 
 public class Main {
 
 	public static void main(String[] args) {
-		int n = 25, m = 2, lambda = 3;
-		double u = 30, p = lambda / u;
+		int n = 4, m = 20, lambda = 15, t = 15;
+		// обрахунок 'u' приводимо до годин:
+		double u = 1D / (t / 60D), p = lambda / u;
 
-		double[] pT = smo_4(n, m, p);
-		System.out.printf("smo_4(n = %s, m = %s, p = %s) = \npT%s\n", n, m, p, Arrays.toString(pT));
-
+		System.out.println("\nОпераційні характеристики СМО із взаємодопомогою між каналами:\n");
+		double[] pT = smo_2_with_help(n, m, p);
+		System.out.printf("smo_2_with_help(n = %s, m = %s, p = %s) = \npT%s\n", n, m, p, Arrays.toString(pT));
 		printCharacteristics(n, m, lambda, u, pT);
-		printTobs(n, m, lambda);
+
+
+		System.out.println("\nОпераційні характеристики СМО без взаємодопомоги між каналами:\n");
+		pT = smo_2_without_help(n, m, p);
+		System.out.printf("smo_2_without_help(n = %s, m = %s, p = %s) = \npT%s\n", n, m, p, Arrays.toString(pT));
+		printCharacteristics(n, m, lambda, u, pT);
 	}
 
-	private static double[] smo_4(int n, int m, double p) {
-		int len = n + 1;
-		double[] pT = new double[len], q = new double[len];
-		q[0] = 1;
-		double s = 1;
+	private static double[] smo_2_with_help(int n, int m, double p) {
+		double[] pT = new double[n + m + 1];
+		double a = p / n;
 
+		pT[0] = (1 - a) / (1 - pow(a, n + m + 1));
+		for (int k = 1; k <= n + m; k++) {
+			pT[k] = pow(a, k) * pT[0];
+		}
+
+		return pT;
+	}
+
+	private static double[] smo_2_without_help(int n, int m, double p) {
+		int len = n + m + 1;
+		double[] pT = new double[len];
+		double[] q = new double[len];
+		q[0] = 1;
+
+		double s = 1;
 		for (int k = 1; k < len; k++) {
-			q[k] = (q[k - 1] * p / min(k, m)) * (n + 1 - k);
+			q[k] = q[k - 1] * p / min(k, n);
 			s += q[k];
 		}
 
-		pT[0] = 1d / s;
-
+		pT[0] = 1 / s;
 		for (int k = 1; k < len; k++) {
 			pT[k] = q[k] * pT[0];
 		}
@@ -40,37 +59,18 @@ public class Main {
 	private static void printCharacteristics(int n, int m, int lambda, double u, double[] pT) {
 		double p = pT[pT.length - 1];
 		double q = 1 - p;
+		double A = q * lambda;
 
 		double avgBusyN = 0;
-		for (int k = 0; k <= n; k++) {
-			avgBusyN += k * pT[k];
+		for (int k = 0; k <= m; k++) {
+			avgBusyN += k * pT[n + k];
 		}
 
-
-		System.out.println("\nОпераційні характеристики СМО:\n");
-		System.out.println("Середнє число зайнятих каналів = " + (1 - pT[0]));
-		System.out.println("Середня кількість не зайнятих каналів = " + (pT[0]));
-		System.out.println("Абсолютна пропускна спроможність = " + avgBusyN * u);
-		System.out.println("Коефіцієнт простою автомобілів = " + avgBusyN / n);
-		System.out.println("Середнє число учнів у системі на обслуговування = " + (n - (1 - pT[0]) / (lambda / u)));
-		System.out.println("Коефіцієнт простою студентів = " + (n - (1 - pT[0]) / (lambda / u)) / n);
-	}
-
-	private static void printTobs(int n, int m, int lambda) {
-		double step = 0.01D;
-		for (double t = 0.1; t > 0; t += step) {
-			double p = lambda * t;
-
-			double[] pT = smo_4(n, m, p);
-			double p0 = pT[0];
-			double n4er = n - ((1 + p) / p) * (1 - p0);
-
-			if (n4er / (lambda * ((1 - p0) / p)) >= 5) {
-				System.out.println("Час обслуговування студентів має бути = " + (t - step) +
-						", щоб час чекання студентом підходу до нього викладача не перевищувала 5 хв");
-				return;
-			}
-		}
+		System.out.println("Імовірність відмови в обслуговуванні = " + p);
+		System.out.println("Відносна пропускна спроможность = " + q);
+		System.out.println("Абсолютна пропускна спроможность = " + A);
+		System.out.println("Середнє число зайнятих каналів = " + (lambda / u) * q);
+		System.out.println("Середня кількість заявок у черзі = " + avgBusyN / n);
 	}
 
 }
